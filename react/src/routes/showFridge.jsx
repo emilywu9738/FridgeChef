0814097fe39f044b22d3ffbddce2b5fd9595eedc
吳妍庭 <wuyenting@yantingdeMacBook-Pro.local>
2024-06-16 15:55:22 +0800

    [feat(react)] show fridge 頁面 - 增加冰箱內容顯示

--- react/src/routes/showFridge.jsx
+++ react/src/routes/showFridge.jsx
@@ -9,12 +9,18 @@ import {
   CardActions,
   CardContent,
   Checkbox,
+  List,
+  ListItem,
   Typography,
 } from '@mui/material';
 
 export default function ShowFridge() {
   const [searchParams] = useSearchParams();
-  const [fridgeData, setFridgeData] = useState({ name: '', members: [] });
+  const [fridgeData, setFridgeData] = useState({
+    name: '',
+    members: [],
+    ingredients: [],
+  });
 
   function MemberCard({ member }) {
     const [isChecked, setIsChecked] = useState(false); // 狀態管理 for each member's checkbox
@@ -33,17 +39,57 @@ export default function ShowFridge() {
           <Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
             Member
           </Typography>
-          <Typography variant='h5' component='div'>
+          <Typography variant='h5' component='div' sx={{ mb: 1 }}>
             {member.name}
           </Typography>
           <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-            偏好: {member.preference}
+            飲食習慣: {member.preference}
           </Typography>
           <Typography variant='body2'>
             排除食材：
             <br />
-            {member.omit.join(', ')}
+            {member.omit.join('、')}
           </Typography>
+        </CardContent>
+      </Card>
+    );
+  }
+
+  function IngredientCard({ category }) {
+    return (
+      <Card sx={{ minWidth: 275, m: 2, overflow: 'visible' }}>
+        <CardContent>
+          <Typography
+            sx={{ fontSize: 18, mb: 1, fontWeight: 500 }}
+            color='text.primary'
+            gutterBottom
+          >
+            {category.category}類
+          </Typography>
+          <List dense>
+            {category.items.map((item) => (
+              <ListItem
+                key={item._id}
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  padding: '8px',
+                  margin: '5px 0',
+                  border: '1px solid #ccc',
+                  borderRadius: '4px',
+                  boxShadow: '0 1px 1px rgba(0,0,0,0.1)',
+                  backgroundColor: 'background.paper',
+                }}
+              >
+                <Typography variant='body2' component='span'>
+                  {item.name}
+                </Typography>
+                <Typography variant='body2' component='span'>
+                  到期日: {new Date(item.expirationDate).toLocaleDateString()}
+                </Typography>
+              </ListItem>
+            ))}
+          </List>
         </CardContent>
       </Card>
     );
@@ -62,14 +108,27 @@ export default function ShowFridge() {
   }, [searchParams]);
 
   return (
-    <>
-      <h1>{fridgeData.name}</h1>
-
-      <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 2 }}>
+    <Box component='div' sx={{ m: 2 }}>
+      <Typography variant='h2' component='div' sx={{ mb: 3 }}>
+        {fridgeData.name}
+      </Typography>
+      <Typography variant='h5' component='div' sx={{ mb: 3 }}>
+        成員名單
+      </Typography>
+      <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 2, mb: 5 }}>
+        <h3></h3>
         {fridgeData.members.map((member) => (
           <MemberCard key={member._id} member={member} />
         ))}
       </Box>
-    </>
+      <Typography variant='h5' component='div' sx={{ mb: 3 }}>
+        食材清單
+      </Typography>
+      <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 2 }}>
+        {fridgeData.ingredients.map((category) => (
+          <IngredientCard key={category._id} category={category} />
+        ))}
+      </Box>
+    </Box>
   );
 }

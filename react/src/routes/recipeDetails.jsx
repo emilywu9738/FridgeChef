d0413df68ba04c626a96d0f556a9f377e8790cdb
吳妍庭 <wuyenting@yantingdeMacBook-Pro.local>
2024-06-28 18:00:44 +0800

    [feat(react)] 修改詳細食譜頁面排版

--- react/src/routes/recipeDetails.jsx
+++ react/src/routes/recipeDetails.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import {
   Box,
   Card,
@@ -9,11 +9,47 @@ import {
   Typography,
   List,
   ListItem,
+  Divider,
+  Button,
 } from '@mui/material';
+import BookmarksIcon from '@mui/icons-material/Bookmarks';
 
 export default function RecipeDetails() {
+  const navigate = useNavigate();
+
   const [recipe, setRecipe] = useState({});
+  const [recipeLikes, setRecipeLikes] = useState(0);
+  const [liked, setLiked] = useState(false);
   const [searchParams] = useSearchParams();
+
+  const handleButtonClick = () => {
+    const recipeId = searchParams.get('id');
+
+    axios
+      .post(
+        'http://localhost:8080/user/updateLikes',
+        { recipeId, liked: !liked },
+        {
+          withCredentials: true,
+        },
+      )
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        if (err.response && err.response.status === 401) {
+          navigate('/login');
+        }
+        console.log(err);
+      });
+
+    if (liked) {
+      setRecipeLikes((prevCount) => prevCount - 1);
+    } else {
+      setRecipeLikes((prevCount) => prevCount + 1);
+    }
+    setLiked(!liked);
+  };
 
   useEffect(() => {
     const recipeId = searchParams.get('id');
@@ -21,49 +57,139 @@ export default function RecipeDetails() {
       .get(`http://localhost:8080/fridge/recipeDetails?id=${recipeId}`)
       .then((response) => {
         setRecipe(response.data);
+        setRecipeLikes(response.data.likes);
       })
       .catch((err) => {
         console.error(err);
       });
-  }, []);
+  }, [searchParams]);
+
+  useEffect(() => {
+    const recipeId = searchParams.get('id');
+    axios
+      .get('http://localhost:8080/user/profile', {
+        withCredentials: true,
+      })
+      .then((response) => {
+        const { liked_recipes } = response.data.userData;
+        const isLiked = liked_recipes.some(
+          (likedRecipe) => likedRecipe === recipeId,
+        );
+        setLiked(isLiked);
+      });
+  }, [searchParams]);
 
   return (
-    <div style={{ backgroundColor: '#faedcd', minHeight: '100vh' }}>
+    <Box style={{ backgroundColor: '#faedcd', minHeight: '100vh' }}>
       <Box sx={{ maxWidth: 800, mx: 'auto' }}>
-        <Card raised>
-          <CardMedia
-            component='img'
-            height='400'
-            image={recipe.coverImage}
-            alt={recipe.title}
-          />
+        <Card elevation={10}>
           <CardContent>
-            <Typography variant='h4' component='div'>
-              {recipe.title}
-            </Typography>
-            <Typography color='text.secondary' gutterBottom>
-              喜歡數：{recipe.likes} | 份量：{recipe.servings}
-            </Typography>
-            <List>
-              {recipe.ingredientsDetail?.map((ingredient, index) => (
-                <ListItem key={index}>{ingredient}</ListItem>
-              ))}
+            <Box display='flex' justifyContent='space-between'>
+              <Typography variant='h4' component='div' sx={{ mt: 1, mb: 2 }}>
+                {recipe.title}
+              </Typography>
+              <Button onClick={handleButtonClick}>
+                <BookmarksIcon
+                  sx={{ color: liked ? 'red' : 'black', fontSize: '35px' }}
+                />
+              </Button>
+            </Box>
+
+            <CardMedia
+              component='img'
+              height='480'
+              image={recipe.coverImage}
+              alt={recipe.title}
+            />
+            <Box
+              sx={{
+                display: 'flex',
+                width: '100%',
+                height: '100px',
+                textAlign: 'center',
+                mt: 1,
+              }}
+            >
+              <Box
+                sx={{
+                  width: '50%',
+                  margin: 'auto',
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}
+              >
+                <Typography color='gray'>已收藏</Typography>
+                <Typography sx={{ mt: '3px', fontSize: '19px' }}>
+                  {recipeLikes} 人
+                </Typography>
+              </Box>
+              <Box
+                sx={{
+                  width: '50%',
+                  margin: 'auto',
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}
+              >
+                <Typography color='gray'>份量</Typography>
+                <Typography sx={{ mt: '3px', fontSize: '19px' }}>
+                  {recipe.servings}
+                </Typography>
+              </Box>
+            </Box>
+            <Divider sx={{ my: 1 }} />
+
+            <Box sx={{ mx: 1 }}>
+              <Typography
+                sx={{ mt: 2, mb: 1, fontSize: '19px', fontWeight: 500 }}
+              >
+                食材
+              </Typography>
+              <Divider sx={{ my: 1 }} />
+              <List
+                sx={{
+                  display: 'grid',
+                  gridTemplateColumns: 'repeat(2, 1fr)',
+                  gap: 1,
+                }}
+              >
+                {recipe.ingredientsDetail?.map((ingredient, index) => (
+                  <ListItem key={index} sx={{ py: '5px', color: 'gray' }}>
+                    {ingredient}
+                  </ListItem>
+                ))}
+              </List>
+            </Box>
+
+            <List sx={{ mt: 3, p: 1 }}>
               {recipe.instructions?.map((step, index) => (
                 <Box key={index} sx={{ mt: 2 }}>
                   <Typography variant='h6'>{`步驟 ${index + 1}`}</Typography>
-                  <Typography>{step.stepText}</Typography>
-                  <CardMedia
-                    component='img'
-                    image={step.stepImage}
-                    alt={`步驟 ${index + 1}`}
-                    sx={{ width: '100%', mt: 1 }}
-                  />
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      mr: 1,
+                      width: '100%',
+                      justifyContent: 'space-between',
+                    }}
+                  >
+                    <Typography sx={{ mt: 1, mr: 1 }}>
+                      {step.stepText}
+                    </Typography>
+                    <CardMedia
+                      component='img'
+                      image={step.stepImage}
+                      alt={`步驟 ${index + 1}`}
+                      sx={{ width: '35%' }}
+                    />
+                  </Box>
+                  <Divider sx={{ mt: 2 }} />
                 </Box>
               ))}
             </List>
           </CardContent>
         </Card>
       </Box>
-    </div>
+    </Box>
   );
 }
